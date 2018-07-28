b1f51d12e01d289f60b30d0e4e618ba18c0a2383
malytinKonstantin <malyytin@gmail.com>
2018-07-28 18:32:49 +0500

    Revert "reacr router"
    This reverts commit 8eec844121748d61c11615e7a2063c6904506129.

--- src/components/ToolbarLink.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
-
-function ToolbarLink({ children, ...rest }) {
-    return <NavLink className="mdc-tab" activeClassName="mdc-tab--active" {...rest}>{children}</NavLink>;
-}
-
-export default ToolbarLink;
